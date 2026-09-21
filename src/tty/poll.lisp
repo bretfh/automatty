593aaaa167399def376d3e10adc9c5f459865a2d
Bret Horne <bretfhorne@gmail.com>
2026-09-20 21:56:25 -0400

    build on mac

--- src/tty/poll.lisp
+++ src/tty/poll.lisp
@@ -2,13 +2,38 @@
 
 (in-package #:atty/tty)
 
+;;; poll, in the shape src/pty/pty.lisp already uses: libc, declared here, with
+;;; the one number that is not the same on every unix in a table of its own.
+;;; struct pollfd is int, short, short on linux, on darwin and on every bsd, and
+;;; POLLIN through POLLNVAL are the same values on all of them, which is why
+;;; sb-unix names them everywhere and the rest of the tree goes on using those.
+
+(sb-alien:define-alien-type nil
+  (sb-alien:struct pollfd
+                   (fd sb-alien:int)
+                   (events sb-alien:short)
+                   (revents sb-alien:short)))
+
+#-(or linux darwin freebsd openbsd netbsd)
+(error "atty/tty has no nfds_t for this system. It is poll's second argument
+in poll.h, and it is the only thing here that is not the same on every unix.")
+
+(sb-alien:define-alien-type nfds-t
+  #+linux sb-alien:unsigned-long
+  #+(or darwin freebsd openbsd netbsd) sb-alien:unsigned-int)
+
+(sb-alien:define-alien-routine ("poll" %poll) sb-alien:int
+  (fds (* (sb-alien:struct pollfd)))
+  (count nfds-t)
+  (milliseconds sb-alien:int))
+
 (defstruct (waiting (:constructor %make-waiting))
   (fds nil)
   (room 0 :type fixnum)
   (count 0 :type fixnum))
 
 (defun make-waiting (&optional (room 8))
-  (%make-waiting :fds (sb-alien:make-alien (sb-alien:struct sb-unix:pollfd) room)
+  (%make-waiting :fds (sb-alien:make-alien (sb-alien:struct pollfd) room)
                  :room room))
 
 (defun free-waiting (w)
@@ -24,13 +49,13 @@
 
 (defun waiting-grow (w room)
   (let ((old (waiting-fds w))
-        (new (sb-alien:make-alien (sb-alien:struct sb-unix:pollfd) room)))
+        (new (sb-alien:make-alien (sb-alien:struct pollfd) room)))
     (dotimes (i (waiting-count w))
       (let ((from (sb-alien:deref old i))
             (to (sb-alien:deref new i)))
-        (setf (sb-alien:slot to 'sb-unix:fd) (sb-alien:slot from 'sb-unix:fd)
-              (sb-alien:slot to 'sb-unix:events) (sb-alien:slot from 'sb-unix:events)
-              (sb-alien:slot to 'sb-unix:revents) (sb-alien:slot from 'sb-unix:revents))))
+        (setf (sb-alien:slot to 'fd) (sb-alien:slot from 'fd)
+              (sb-alien:slot to 'events) (sb-alien:slot from 'events)
+              (sb-alien:slot to 'revents) (sb-alien:slot from 'revents))))
     (when old (sb-alien:free-alien old))
     (setf (waiting-fds w) new
           (waiting-room w) room))
@@ -40,14 +65,14 @@
   (when (>= (waiting-count w) (waiting-room w))
     (waiting-grow w (max 8 (* 2 (waiting-room w)))))
   (let ((slot (sb-alien:deref (waiting-fds w) (waiting-count w))))
-    (setf (sb-alien:slot slot 'sb-unix:fd) fd
-          (sb-alien:slot slot 'sb-unix:events) events
-          (sb-alien:slot slot 'sb-unix:revents) 0))
+    (setf (sb-alien:slot slot 'fd) fd
+          (sb-alien:slot slot 'events) events
+          (sb-alien:slot slot 'revents) 0))
   (prog1 (waiting-count w)
     (incf (waiting-count w))))
 
 (defun waiting-back (w n)
-  (sb-alien:slot (sb-alien:deref (waiting-fds w) n) 'sb-unix:revents))
+  (sb-alien:slot (sb-alien:deref (waiting-fds w) n) 'revents))
 
 (defun readable-p (back)
   (plusp (logand back (logior sb-unix:pollin sb-unix:pollhup sb-unix:pollerr))))
@@ -66,11 +91,11 @@ A signal reaching this process interrupts the wait and is not a fault: it is
 how the terminal says it was resized, and the loop above has to see it."
   (when (zerop (waiting-count w))
     (return-from wait-on 0))
-  (multiple-value-bind (n errno)
-      (sb-unix:unix-poll (sb-alien:addr (sb-alien:deref (waiting-fds w) 0))
-                         (waiting-count w)
-                         milliseconds)
-    (cond
-      (n n)
-      ((eql errno sb-unix:eintr) nil)
-      (t (error "poll: ~A" (sb-int:strerror errno))))))
+  (let ((n (%poll (sb-alien:addr (sb-alien:deref (waiting-fds w) 0))
+                  (waiting-count w)
+                  milliseconds)))
+    (if (minusp n)
+        (let ((errno (sb-alien:get-errno)))
+          (unless (eql errno sb-unix:eintr)
+            (error "poll: ~A" (sb-int:strerror errno))))
+        n)))
